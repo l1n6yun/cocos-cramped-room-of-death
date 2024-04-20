f07cc2fe1116f7f86ae33839472e19914da86f0c
l1n6yun <l1n6yun@gmail.com>
2024-04-20 21:45:59 +0800

    12 - 实现Player人物移动

--- assets/Script/Player/PlayerManager.ts
+++ assets/Script/Player/PlayerManager.ts
@@ -5,7 +5,7 @@ import Levels, { ILevel } from 'db://assets/Levels'
 import DataManager from 'db://assets/Runtime/DataManager'
 import { TILE_HEIGHT, TILE_WIDTH } from 'db://assets/Script/Tile/TileManager'
 import EventManager from 'db://assets/Runtime/EventManager'
-import { EVENT_ENUM } from 'db://assets/Enums'
+import { CONTROLLER_ENUM, EVENT_ENUM } from 'db://assets/Enums'
 import ResourceManager from 'db://assets/Runtime/ResourceManager'
 
 const { ccclass, property } = _decorator
@@ -14,7 +14,56 @@ const ANIMATION_SPEED = 1 / 8
 
 @ccclass('PlayerManager')
 export class PlayerManager extends Component {
+  x: number = 0
+  y: number = 0
+  targetX: number = 0
+  targetY: number = 0
+  private readonly speed = 1 / 10
+
+
   async init() {
+    await this.render()
+
+    EventManager.Instance.on(EVENT_ENUM.PLAYER_CTRL, this.move, this)
+  }
+
+  update() {
+    this.updateXY()
+    this.node.setPosition(this.x * TILE_WIDTH - TILE_WIDTH * 1.5, -this.y * TILE_HEIGHT + TILE_HEIGHT * 1.5)
+  }
+
+  updateXY() {
+    if (this.targetX < this.x) {
+      this.x -= this.speed
+    } else if (this.targetX > this.x) {
+      this.x += this.speed
+    }
+
+    if (this.targetY < this.y) {
+      this.y -= this.speed
+    } else if (this.targetY > this.y) {
+      this.y += this.speed
+    }
+
+    if (Math.abs(this.targetX - this.x) <= 0.1 && Math.abs(this.targetY - this.y) <= 0.1) {
+      this.x = this.targetX
+      this.y = this.targetY
+    }
+  }
+
+  move(inputDirection: CONTROLLER_ENUM) {
+    if (inputDirection === CONTROLLER_ENUM.TOP) {
+      this.targetY -= 1
+    } else if (inputDirection === CONTROLLER_ENUM.BOTTOM) {
+      this.targetY += 1
+    } else if (inputDirection === CONTROLLER_ENUM.LEFT) {
+      this.targetX -= 1
+    } else if (inputDirection === CONTROLLER_ENUM.RIGHT) {
+      this.targetX += 1
+    }
+  }
+
+  async render() {
     const sprite = this.addComponent(Sprite)
     sprite.sizeMode = Sprite.SizeMode.CUSTOM
 
